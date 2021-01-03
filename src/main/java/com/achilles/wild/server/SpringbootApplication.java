6ce799d6c1d16924613ee17e4081e69f28afbd03
AchillesWild <532398999@qq.com>
2021-01-03 23:44:36 +0800

    test

--- src/main/java/com/achilles/wild/server/SpringbootApplication.java
+++ src/main/java/com/achilles/wild/server/SpringbootApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 //@SpringBootApplication
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan(basePackages = {"com.achilles.wild.server"})
-//@ImportResource("classpath:application.xml")
+@ImportResource("classpath:applicationContext.xml")
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
