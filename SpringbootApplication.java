0d935254cff7483c42d7a27329441ee73007cb54
AchillesWild <56060020+AchillesWild@users.noreply.github.com>
2021-01-03 11:12:11 +0800

    Add files via upload

---
+++ SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.achilles.wild.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan(basePackages = {"com.achilles.wild.server"})
+//@ImportResource("classpath:application.xml")
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
