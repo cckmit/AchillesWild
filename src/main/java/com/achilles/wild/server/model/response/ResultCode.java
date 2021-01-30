25f49f55bb7c6e748e71d10a6a319749ebb1049e
AchillesWild <532398999@qq.com>
2021-01-30 16:38:13 +0800

    exception

--- src/main/java/com/achilles/wild/server/model/response/ResultCode.java
+++ src/main/java/com/achilles/wild/server/model/response/ResultCode.java
@@ -16,13 +16,13 @@ public enum ResultCode implements Serializable {
     DATA_HAS_EXISTS("-107", "data has exists: %s"),
 
 
-    BALANCE_NOT_ENOUGH("-600", "balance not enough ~"),
+    BALANCE_NOT_ENOUGH("600", "balance not enough ~"),
 
-    TOO_MANY_REQUESTS("-700", "too many requests ~ : %s"),
 
-    REQUESTS_TOO_FREQUENT("-700", "requests are too frequent ~ : %s"),
+    EXCEPTION_TO_CLIENT("-1000", "the server is too busy ~ : %s"),
+    TOO_MANY_REQUESTS("-1001", "too many requests ~ : %s"),
+    REQUESTS_TOO_FREQUENT("-1002", "requests are too frequent ~ : %s");
 
-    EXCEPTION_TO_CLIENT("-1000", "the server is too busy ~ : %s");
 
     public final String code;
     public final String message;
