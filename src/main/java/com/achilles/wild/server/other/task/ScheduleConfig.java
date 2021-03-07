1ef810f8e68eac66c9cd09f7364e03166509cda6
AchillesWild <532398999@qq.com>
2021-03-07 13:12:28 +0800

    task config

---
+++ src/main/java/com/achilles/wild/server/other/task/ScheduleConfig.java
@@ -0,0 +1,21 @@
+package com.achilles.wild.server.other.task;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+/**
+ * 让定时任务多线程并行执行，不阻塞（默认单线程）
+ */
+@Configuration
+public class ScheduleConfig {
+
+    @Bean
+    public ScheduledThreadPoolExecutor scheduledExecutorService() {
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(2,
+                new ThreadFactoryBuilder().setNameFormat("schedule_pool_worker_%d").build());
+        return executor;
+    }
+}
--- src/main/java/com/achilles/wild/server/other/task/ScheduleTask.java
+++ src/main/java/com/achilles/wild/server/other/task/ScheduleTask.java
@@ -18,12 +18,21 @@ public class ScheduleTask {
     //3.添加定时任务
 //    @Scheduled(cron = "0/5 * * * * ?")
     //或直接指定时间间隔，例如：5秒
-    @Scheduled(fixedRate=2000)
+    @Scheduled(fixedRate=20000)
     private void configureTasks() {
         String traceId = GenerateUniqueUtil.getTraceId("task1");
         MDC.put(CommonConstant.TRACE_ID,traceId);
         log.debug("---------task---------traceId  : " + traceId);
 
+        MDC.remove(CommonConstant.TRACE_ID);
+    }
+
+    @Scheduled(fixedRate=200000)
+    private void configureTasks2() throws Exception{
+        String traceId = GenerateUniqueUtil.getTraceId("task1");
+        MDC.put(CommonConstant.TRACE_ID,traceId);
+        log.debug("---------task2---------traceId  : " + traceId);
+        Thread.sleep(5000L);
         MDC.remove(CommonConstant.TRACE_ID);
     }
 }
