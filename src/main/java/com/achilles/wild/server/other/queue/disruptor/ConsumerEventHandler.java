d69027585812d3cb38d2e4cbafdb20a6e7bab6cb
AchillesWild <532398999@qq.com>
2021-03-16 22:12:01 +0800

    consume

--- src/main/java/com/achilles/wild/server/other/queue/disruptor/ConsumerEventHandler.java
+++ src/main/java/com/achilles/wild/server/other/queue/disruptor/ConsumerEventHandler.java
@@ -1,9 +1,16 @@
 package com.achilles.wild.server.other.queue.disruptor;
 
+import com.achilles.wild.server.business.manager.common.LogTimeInfoManager;
 import com.achilles.wild.server.entity.common.LogTimeInfo;
+import com.achilles.wild.server.tool.SpringContextUtil;
+import com.achilles.wild.server.tool.json.JsonUtil;
 import com.lmax.disruptor.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 消费者
@@ -12,19 +19,36 @@ public class ConsumerEventHandler implements EventHandler<LogTimeInfo> {
 
     private final static Logger log = LoggerFactory.getLogger(ConsumerEventHandler.class);
 
+//    @Autowired
+//    LogTimeInfoManager logTimeInfoManager;
+
+    private final List<LogTimeInfo> logTimeInfoList = new ArrayList<>();
+
     @Override
-    public void onEvent(LogTimeInfo event, long sequence, boolean endOfBatch) {
-        try {
-            log.info("消费者消费的信息是：sequence:"+sequence+",endOfBatch:"+endOfBatch);
-            //这里停止1000ms是为了确定消费消息是异步的
+    public void onEvent(LogTimeInfo logTimeInfo, long sequence, boolean endOfBatch) {
+
+        log.info("消费者消费的信息是：sequence:"+sequence+",endOfBatch:"+endOfBatch+",event:"+ JsonUtil.toJsonString(logTimeInfo));
+        //这里停止1000ms是为了确定消费消息是异步的
 //            Thread.sleep(1000);
-            log.info("消费者处理消息开始");
-            if (event != null) {
-                log.info("消费者消费的信息是：{}",event);
-            }
-        } catch (Exception e) {
-            log.info("消费者处理消息失败");
+        log.info("消费者处理消息开始");
+        if (logTimeInfo == null) {
+            log.error("----------------------------空消息-------------------------sequence:"+sequence);
+            return;
         }
+
+        logTimeInfoList.add(logTimeInfo);
+        try {
+            if (logTimeInfoList.size()==1){
+                LogTimeInfoManager logTimeInfoManager = (LogTimeInfoManager) SpringContextUtil.getBean(LogTimeInfoManager.class);
+                logTimeInfoManager.addLogs(logTimeInfoList);
+
+                logTimeInfoList.clear();
+            }
+        } catch (BeansException e) {
+            e.printStackTrace();
+        }
+
+
         log.info("消费者处理消息结束");
     }
 
--- src/main/java/com/achilles/wild/server/tool/SpringContextUtil.java
+++ src/main/java/com/achilles/wild/server/tool/SpringContextUtil.java
@@ -4,7 +4,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
- 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringContextUtil implements ApplicationContextAware {
 	
   private static ApplicationContext applicationContext;     //Spring应用上下文环境
@@ -14,8 +16,8 @@ public class SpringContextUtil implements ApplicationContextAware {
   * @param applicationContext
   * @throws BeansException
   */
-  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {	
-      SpringContextUtil.applicationContext = applicationContext;
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    SpringContextUtil.applicationContext = applicationContext;
   }
  
   /**
@@ -23,9 +25,20 @@ public class SpringContextUtil implements ApplicationContextAware {
   */
   public static ApplicationContext getApplicationContext() 
    {
-    return SpringContextUtil.applicationContext;
+    return applicationContext;
   }
- 
+
+  /**
+   * 获取bean
+   *
+   * @param clz
+   * @return
+   * @throws BeansException
+   */
+  public static <T> T getBean(Class<T> clz) throws BeansException {
+    return applicationContext.getBean(clz);
+  }
+
   /**
   * 获取对象   
   * 
