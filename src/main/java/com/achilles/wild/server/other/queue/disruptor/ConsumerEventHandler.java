e0921a67f99b9e4c13efdd19b0919fc04d4703a1
AchillesWild <532398999@qq.com>
2021-03-17 22:11:38 +0800

    task

--- src/main/java/com/achilles/wild/server/other/queue/disruptor/ConsumerEventHandler.java
+++ src/main/java/com/achilles/wild/server/other/queue/disruptor/ConsumerEventHandler.java
@@ -3,13 +3,19 @@ package com.achilles.wild.server.other.queue.disruptor;
 import com.achilles.wild.server.business.manager.common.LogTimeInfoManager;
 import com.achilles.wild.server.entity.common.LogTimeInfo;
 import com.achilles.wild.server.tool.SpringContextUtil;
+import com.achilles.wild.server.tool.date.DateUtil;
 import com.achilles.wild.server.tool.json.JsonUtil;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.lmax.disruptor.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 消费者
@@ -21,6 +27,9 @@ public class ConsumerEventHandler implements EventHandler<LogTimeInfo> {
 
     private LogTimeInfoManager logTimeInfoManager;
 
+
+    private Date lastUpdateTime;
+
     private LogTimeInfoManager getLogTimeInfoManager() {
         if (logTimeInfoManager != null){
             return logTimeInfoManager;
@@ -30,6 +39,8 @@ public class ConsumerEventHandler implements EventHandler<LogTimeInfo> {
     }
 
     private final List<LogTimeInfo> logTimeInfoList = new ArrayList<>();
+
+    private Thread.State threadState;
 
     @Override
     public void onEvent(LogTimeInfo logTimeInfo, long sequence, boolean endOfBatch) {
@@ -41,18 +52,62 @@ public class ConsumerEventHandler implements EventHandler<LogTimeInfo> {
             return;
         }
 
-        logTimeInfoList.add(logTimeInfo);
-        try {
+        if (threadState==null){
+            doIt();
+        }
 
-            if (logTimeInfoList.size() == 1){
+        logTimeInfoList.add(logTimeInfo);
+        lastUpdateTime = DateUtil.getCurrentDate();
+        if (logTimeInfoList.size() < 5){
+            return;
+        }
+        try {
+            synchronized (logTimeInfoList){
+                if (logTimeInfoList.size() != 5) {
+                    return;
+                }
                 getLogTimeInfoManager().addLogs(logTimeInfoList);
                 logTimeInfoList.clear();
+
             }
         } catch (Exception e) {
             e.printStackTrace();
             log.error("----------------------------insert into db error-------------------------");
         }
+    }
 
+    private void doIt(){
+
+        log.debug("-----Disruptor--consumer   task -start-------");
+
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor(
+                new ThreadFactoryBuilder().setNameFormat("disruptor_log_%d").build());
+
+        service.scheduleAtFixedRate(()->{
+
+            threadState = Thread.currentThread().getState();
+            if (DateUtil.getGapSeconds(lastUpdateTime) <= 10){
+                return;
+            }
+            if (logTimeInfoList.size() == 0) {
+                return;
+            }
+
+            try {
+                synchronized (logTimeInfoList){
+                    if (logTimeInfoList.size() == 0) {
+                        return;
+                    }
+                    getLogTimeInfoManager().addLogs(logTimeInfoList);
+                    logTimeInfoList.clear();
+                    lastUpdateTime = DateUtil.getCurrentDate();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.error("-----Disruptor--consumer   task--- :"+e.getMessage());
+            }
+
+        }, 5, 10, TimeUnit.SECONDS);
     }
 
 }
