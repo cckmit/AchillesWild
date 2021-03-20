f6631b7e576490374101637170d932617da5b75d
AchillesWild <532398999@qq.com>
2021-03-20 21:54:59 +0800

    flow

--- src/main/java/com/achilles/wild/server/common/aop/limit/FlowLimit.java
+++ src/main/java/com/achilles/wild/server/common/aop/limit/FlowLimit.java
@@ -1,6 +1,8 @@
 package com.achilles.wild.server.common.aop.limit;
 
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +14,13 @@ import java.util.List;
 @Configuration
 public class FlowLimit {
 
+    String key = "limit_test";
+
     @PostConstruct
     public void initFlowQpsRule() {
         List<FlowRule> rules = new ArrayList<FlowRule>();
         FlowRule rule1 = new FlowRule();
-        rule1.setResource("limit_test");
+        rule1.setResource(key);
         // QPS控制在2以内
         rule1.setCount(1);
         // QPS限流
@@ -24,6 +28,19 @@ public class FlowLimit {
         rule1.setLimitApp("default");
         rules.add(rule1);
         FlowRuleManager.loadRules(rules);
+    }
+
+    @PostConstruct
+    public void initDegradeRule() {
+        List<DegradeRule> rules = new ArrayList<>();
+        DegradeRule rule = new DegradeRule();
+        rule.setResource(key);
+        // 80s内调用接口出现异常次数超过5的时候, 进行熔断
+        rule.setCount(5);
+        rule.setGrade(RuleConstant.DEGRADE_GRADE_EXCEPTION_COUNT);
+        rule.setTimeWindow(80);
+        rules.add(rule);
+        DegradeRuleManager.loadRules(rules);
     }
 
 }
