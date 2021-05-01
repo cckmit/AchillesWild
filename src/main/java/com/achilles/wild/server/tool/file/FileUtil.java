8c97ac2fc6467d562b76db392e4c799b952d4cec
AchillesWild <532398999@qq.com>
2021-05-01 19:51:37 +0800

    文件处理

--- src/main/java/com/achilles/wild/server/tool/file/FileUtil.java
+++ src/main/java/com/achilles/wild/server/tool/file/FileUtil.java
@@ -1,11 +1,65 @@
 package com.achilles.wild.server.tool.file;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
+import java.util.Base64;
 
 public class FileUtil {
+
+
+    /**
+     * cloneInputStream
+     *
+     * @param inputStream
+     * @param size
+     * @return
+     */
+    public static ByteArrayInputStream[] cloneInputStream(InputStream inputStream,int size) {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        try {
+            while ((len = inputStream.read(buffer)) > -1) {
+                byteArrayOutputStream.write(buffer, 0, len);
+            }
+            byteArrayOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        ByteArrayInputStream[] byteArrayInputStreams = new ByteArrayInputStream[size];
+        for (int i = 0; i < size; i++) {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+            byteArrayInputStreams[i] = byteArrayInputStream;
+        }
+        return byteArrayInputStreams;
+    }
+
+    /**
+     * toBase64
+     *
+     * @param inputStream
+     * @return
+     */
+    public static String toBase64(InputStream inputStream){
+
+        byte[] bytes;
+        try {
+            bytes = IOUtils.toByteArray(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        String encoded = Base64.getEncoder().encodeToString(bytes);
+
+        return encoded;
+    }
 
     /**
      * toInputStream
@@ -19,11 +73,12 @@ public class FileUtil {
             throw new IllegalArgumentException("path can not be null !");
         }
 
-        FileInputStream inputStream = null;
+        FileInputStream inputStream;
         try {
-            inputStream = new FileInputStream(new File(path));
+            inputStream = new FileInputStream(path);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+            return null;
         }
 
         return inputStream;
@@ -36,7 +91,7 @@ public class FileUtil {
      * @param path "C:\\Users\\Achilles\\Desktop\\z.jpg";
      * @return
      */
-    public static InputStream toFile(InputStream inputStream,String path){
+    public static void toFile(InputStream inputStream,String path){
 
         if (inputStream == null) {
             throw new IllegalArgumentException("inputStream can not be null !");
@@ -52,6 +107,5 @@ public class FileUtil {
             e.printStackTrace();
         }
 
-        return inputStream;
     }
 }
