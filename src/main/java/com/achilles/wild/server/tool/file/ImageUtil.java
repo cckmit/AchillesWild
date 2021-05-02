f1632e82e64da9c14eacb890c5e946ccffeedbd8
AchillesWild <532398999@qq.com>
2021-05-02 22:26:45 +0800

    image width height

--- src/main/java/com/achilles/wild/server/tool/file/ImageUtil.java
+++ src/main/java/com/achilles/wild/server/tool/file/ImageUtil.java
@@ -7,10 +7,12 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ImageUtil {
 
-    static String srcPath = "C:\\Users\\Achilles\\Desktop\\photo\\4609.jpg";
+    static String srcPath = "C:\\Users\\Achilles\\Desktop\\photo\\10028.jpg";
     static String destPath = "C:\\Users\\Achilles\\Desktop\\test.jpg";
 
     static String format = "jpg";
@@ -26,7 +28,8 @@ public class ImageUtil {
 
 //        compressForScale(path,desc,220*1024L,0.8);
 
-        trimBySizeLimit(srcPath,destPath,300);
+//        trimBySizeLimit(srcPath,destPath,300);
+        System.out.println(getWidthAndHeight(srcPath));
     }
 
     /**
@@ -100,46 +103,27 @@ public class ImageUtil {
     }
 
     /**
+     * getWidthAndHeight
      *
-     * @param srcPath 原图片地址
-     * @param desPath 目标图片地址
-     * @param desFileSize 指定图片大小,单位kb
-     * @param accuracy 精度,递归压缩的比率,建议小于0.9
+     * @param desPath
      * @return
      */
-    public static String compressForScale(String srcPath,String desPath,long desFileSize , double accuracy){
-        try {
-            File srcFile = new File(srcPath);
-            long srcFilesize = srcFile.length();
-            System.out.println("原图片:"+srcPath + ",大小:" + srcFilesize/1024 + "kb");
-            //递归压缩,直到目标文件大小小于desFileSize
-            compressPicCycle(desPath, desFileSize, accuracy);
+    public static Map<String,Integer> getWidthAndHeight(String desPath) {
 
-            File desFile = new File(desPath);
-            System.out.println("目标图片:" + desPath + ",大小" + desFile.length()/1024 + "kb");
-            System.out.println("图片压缩完成!");
-        } catch (Exception e) {
+        BufferedImage bim = null;
+        try {
+            bim = ImageIO.read(new FileInputStream(srcPath));
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return desPath;
-    }
+        int width = bim.getWidth();
+        int height = bim.getHeight();
 
-    public static void compressPicCycle(String desPath , long desFileSize, double accuracy) throws IOException{
-        File imgFile = new File(desPath);
-        long fileSize = imgFile.length();
-        //判断大小,220,不压缩,如果大于等于220k,压缩
-        if(fileSize <= desFileSize * 300){
-            return;
-        }
-        //计算宽高
-        BufferedImage bim = ImageIO.read(imgFile);
-        int imgWidth = bim.getWidth();
-        int imgHeight = bim.getHeight();
-        int desWidth = new BigDecimal(imgWidth).multiply(new BigDecimal(accuracy)).intValue();
-        int desHeight = new BigDecimal(imgHeight).multiply(new BigDecimal(accuracy)).intValue();
-        Thumbnails.of(desPath).size(desWidth, desHeight).outputQuality(accuracy).toFile(desPath);
-        //如果不满足要求,递归直至满足小于1M的要求
-        compressPicCycle(desPath, desFileSize, accuracy);
+        Map<String,Integer> map = new HashMap<>();
+        map.put("width",width);
+        map.put("height",height);
+
+        return map;
     }
 
     /**
