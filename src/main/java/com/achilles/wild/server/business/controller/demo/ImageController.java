d229967bcbadfdd9a3304598933ef01fabe1f196
AchillesWild <532398999@qq.com>
2021-05-05 01:17:33 +0800

    image test

--- src/main/java/com/achilles/wild/server/business/controller/demo/ImageController.java
+++ src/main/java/com/achilles/wild/server/business/controller/demo/ImageController.java
@@ -1,7 +1,12 @@
 package com.achilles.wild.server.business.controller.demo;
 
 import com.achilles.wild.server.common.aop.log.annotation.IgnoreParams;
+import com.achilles.wild.server.tool.file.FileUtil;
+import com.achilles.wild.server.tool.file.ImageUtil;
+import com.achilles.wild.server.tool.generate.encrypt.MD5Util;
+import com.achilles.wild.server.tool.generate.unique.GenerateUniqueUtil;
 import net.coobird.thumbnailator.Thumbnails;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,32 +14,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping(value = "/image")
 public class ImageController {
 
-    private final static Logger LOG = LoggerFactory.getLogger(ImageController.class);
+    private final static Logger log = LoggerFactory.getLogger(ImageController.class);
+
+    static String srcPath = "C:\\Users\\Achilles\\Desktop\\photo\\test\\10028.jpg";
 
     @PostMapping("/upload")
     @IgnoreParams
     public String upload(MultipartFile file){
 
-        if (file.getSize()>30*1024*1024){
-            return "太大";
+        InputStream inputStream = null;
+        String key = null;
+        try {
+            inputStream = file.getInputStream();
+            ByteArrayInputStream[] byteArrayInputStreams = FileUtil.cloneInputStream(inputStream,2);
+            log.info("before trimBySizeLimit");
+
+            InputStream trimInputStream = ImageUtil.trimBySizeLimit(byteArrayInputStreams[0],300);
+            log.info("after trimBySizeLimit");
+            FileUtil.toFile(trimInputStream,"C:\\Users\\Achilles\\Desktop\\photo\\test\\"+ GenerateUniqueUtil.getUuId() +".jpg");
+            log.info("after toFile");
+            key = MD5Util.getAddSalt(byteArrayInputStreams[1]);
+            log.info("after MD5Util");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(inputStream);
         }
 
-        if (file.getSize()<=500*1024){
-            return "太小，不用压缩";
-        }
 
-        File uploadFile=new File("D:/image/");
+//        if (file.getSize()>30*1024*1024){
+//            return "太大";
+//        }
+//
+//        if (file.getSize()<=500*1024){
+//            return "太小，不用压缩";
+//        }
+//
+//        File uploadFile=new File("D:/image/");
+//
+//        String path = thumbnails( uploadFile, file, 200);
 
-        String path = thumbnails( uploadFile, file, 200);
 
-        return path;
+//        file.transferTo();
+        return key;
     }
 
 
