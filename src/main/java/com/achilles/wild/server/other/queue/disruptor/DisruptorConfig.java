fa6f768e709ff5b41b3873edb2c8c0b60995c695
AchillesWild <532398999@qq.com>
2021-06-10 00:21:11 +0800

    队列 测试

--- src/main/java/com/achilles/wild/server/other/queue/disruptor/DisruptorConfig.java
+++ src/main/java/com/achilles/wild/server/other/queue/disruptor/DisruptorConfig.java
@@ -13,8 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DisruptorConfig {
 
-    //2^20 1048576
-    @Value("${disruptor.ring.buffer.size:1024}")//2^13指定 RingBuffer 大小，必须为2的N次方（能将求模运算转为位运算提高效率），否则将影响效率
+    //2^20 1048576 2^14 16384
+    @Value("${disruptor.ring.buffer.size:8}")//2^13指定 RingBuffer 大小，必须为2的N次方（能将求模运算转为位运算提高效率），否则将影响效率
     private Integer bufferSize;
 
 
